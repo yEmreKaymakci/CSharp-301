4b799f6f480e536237e87aec28398e2bfc8fa194
yEmreKaymakci <yunusemrekymkci@gmail.com>
2025-11-04 03:00:50 +0300

    Adding FrmProduct form

--- CSharpEgitimKampı301.PresentationLayer/Program.cs
+++ CSharpEgitimKampı301.PresentationLayer/Program.cs
@@ -16,7 +16,7 @@ namespace CSharpEgitimKampı301.PresentationLayer
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FrmCategory());
+            Application.Run(new FrmProduct());
         }
     }
 }
